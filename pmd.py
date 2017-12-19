ef786510d90a920f66444250778a61fe76382f29
Rimo <RimoChan@stu.xmu.edu.cn>
2017-12-19 10:44:58 +0800

    优化了代码，加入了「_」功能。

--- pmd.py
+++ pmd.py
@@ -1,10 +1,28 @@
-import os,sys
+import os
 import shlex
+
 import 光標
-import 命令
 os.system('title pmd')
 
-while True:
+def 跑命令(字串):
+    import 命令
+    if os.path.isdir(字串):
+        os.chdir(字串)
+        return
+        
+    a=shlex.split(字串.replace('\\','/'))
+    首=a[0]
+    if 首 in dir(命令):
+        try:
+            m(*a[1:])
+        except BaseException as e:
+            print(e)
+    elif os.path.isfile(首) and 首.split('.')[-1]=='py':            
+        os.system('python '+字串)
+    else:
+        os.system(字串)
+    
+def 畫上條(): 
     p=光標.位置()
     光標.去頂()
     print('#'*60)
@@ -15,27 +33,16 @@ while True:
     if 光標.位置()[1]<3:
         光標.去(0,3)
 
+while True:
+    畫上條()
 
-    t=input('>>')
+    _输入=input('>>')
     try:
-        k=eval(t)
-        if k!=None:
-            print(k)
+        _=eval(_输入)
+        if _!=None:
+            print(_)
     except:
         try:
-            exec(t)
+            exec(_输入)
         except:
-            try:
-                os.chdir(t)
-            except:
-                try:
-                    a=shlex.split(t.replace('\\','/'))
-                    m=eval('命令.%s'%a[0])
-                except BaseException as e:
-                    m=None
-                    os.system(t)
-                if m:
-                    try:
-                        m(*a[1:])
-                    except BaseException as e:
-                        print(e)
+            跑命令(_输入)
